219f0c7c803c9e37c48cf8a9ad33a88512c8762a
Amir Ossanloo <6134758+AmirOssanloo@users.noreply.github.com>
2020-10-24 15:03:24 +0200

    Add working with files and directories

---
+++ 001-files-and-directories/index.php
@@ -0,0 +1,28 @@
+<?php
+  define("ALBUMS_PATH", "./albums");
+
+  function getFiles($path) {
+    $files = array_diff(scandir($path), array('.', '..'));
+    $result = [];
+
+    if (count($files) > 0) {
+      foreach ($files as $file) {
+        $filePath = "$path/$file";
+
+        if (is_file($filePath)) {
+          $fileLinesList = file($filePath) or die("ERROR: Cannot open the file.");
+          return $fileLinesList;
+        } else {
+          $result[$file] = getFiles($filePath);
+        }
+      }
+    } else {
+      echo "ERROR: No files found in the directory.";
+    }
+
+    return $result;
+  }
+
+  $albums = getFiles(ALBUMS_PATH);
+  print_r($albums["Dimmu Borgir"]["Abrahadabra"]);
+?>
